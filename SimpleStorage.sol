9fda367eeeb311056277ba82feedff45dcf66972
cihuyyama <miqbalalhabib@gmail.com>
2023-08-12 15:43:23 +0700

    learn inherit

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,26 +2,26 @@
 pragma solidity ^0.8.18;
 
 contract SimpleStorage {
-    int myfav;
+    uint256 myfav;
 
     struct Person {
         string name;
-        int fav;
+        uint256 fav;
     }
 
     Person[] public listOfPerson;
 
-    mapping(string => int) public nametofav;
+    mapping(string => uint256) public nametofav;
 
-    function store(int _myfav) public {
+    function store(uint256 _myfav) public virtual  {
         myfav = _myfav;
     }
 
-    function retrieve() public view returns (int) {
+    function retrieve() public view returns (uint256) {
         return myfav;
     }
 
-    function addPerson(string memory _name, int _fav) public {
+    function addPerson(string memory _name, uint256 _fav) public {
         listOfPerson.push(Person(_name, _fav));
         nametofav[_name] = _fav;
     }
